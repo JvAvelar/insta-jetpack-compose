7c958f2322597433a28f3176a60ef3690827870a
João Vitor Avelar <jvitoravelar1200@gmail.com>
2025-05-04 01:04:42 -0300

    alterado os nomes dos métodos que geram dados

--- app/src/main/java/com/jvitor/jpcpratice/viewmodel/ScreenViewModel.kt
+++ app/src/main/java/com/jvitor/jpcpratice/viewmodel/ScreenViewModel.kt
@@ -2,50 +2,40 @@ package com.jvitor.jpcpratice.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.jvitor.jpcpratice.R
-import com.jvitor.jpcpratice.data.model.Destaque
-import com.jvitor.jpcpratice.data.model.Postagem
+import com.jvitor.jpcpratice.data.model.Feature
+import com.jvitor.jpcpratice.data.model.Post
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class ScreenViewModel : ViewModel() {
 
-    private val _postagem = MutableStateFlow<List<Postagem>>(emptyList())
-    val postagem: StateFlow<List<Postagem>> = _postagem.asStateFlow()
+    private val _post = MutableStateFlow<List<Post>>(emptyList())
+    val post: StateFlow<List<Post>> = _post.asStateFlow()
 
-    private val _destaque = MutableStateFlow<List<Destaque>>(emptyList())
-    val destaque: StateFlow<List<Destaque>> = _destaque.asStateFlow()
+    private val _featured = MutableStateFlow<List<Feature>>(emptyList())
+    val featured: StateFlow<List<Feature>> = _featured.asStateFlow()
 
-    fun recuperarPostagem(){
-        val listaPostagem = listOf(
-            Postagem(R.drawable.perfil_01, "João", R.drawable.android, "Androidão é daora demais!", 2),
-            Postagem(
-                R.drawable.perfil_02,
-                "Luiz",
-                R.drawable.floresta,
-                "Visitei a floresta e foi muito Legal!"
-            ),
-            Postagem(
-                R.drawable.perfil_03,
-                "Luana",
-                R.drawable.praia,
-                "Visitei a praia e foi muito daora!",
-                2
-            ),
-            Postagem(R.drawable.perfil_01, "Zyon", R.drawable.android, "Android é bom demais!", 3),
+    fun getPost(){
+        val listPost = listOf(
+            Post(R.drawable.perfil_01, "João", R.drawable.android, "Androidão é daora demais!", 2),
+            Post(R.drawable.perfil_02, "Luiz", R.drawable.floresta, "Visitei a floresta e foi muito Legal!"),
+            Post(R.drawable.perfil_03, "Luana", R.drawable.praia, "Visitei a praia e foi muito daora!", 2),
+            Post(R.drawable.perfil_01, "Zyon", R.drawable.android, "Android é bom demais!", 3),
         )
-        _postagem.value = listaPostagem
+        _post.value = listPost
     }
 
-    fun recuperarDestaque(){
-        val listaDestaques = listOf(
-            Destaque(R.drawable.perfil_01, "João"),
-            Destaque(R.drawable.perfil_02, "Luiz"),
-            Destaque(R.drawable.perfil_03, "Luana"),
-            Destaque(R.drawable.perfil_01, "Zyon"),
-            Destaque(R.drawable.perfil_02, "Vitor"), Destaque(R.drawable.perfil_03, "Duarte"),
-            Destaque(R.drawable.perfil_01, "Avelar"),
+    fun getFeatured(){
+        val listFeatured = listOf(
+            Feature(profilePhoto = R.drawable.perfil_01, name = "João"),
+            Feature(profilePhoto = R.drawable.perfil_02, name = "Luiz"),
+            Feature(profilePhoto = R.drawable.perfil_03, name = "Luana"),
+            Feature(profilePhoto = R.drawable.perfil_01, name = "Zyon"),
+            Feature(profilePhoto = R.drawable.perfil_02, name = "Vitor"),
+            Feature(profilePhoto = R.drawable.perfil_03, name = "Duarte"),
+            Feature(profilePhoto = R.drawable.perfil_01, name = "Avelar"),
         )
-        _destaque.value = listaDestaques
+        _featured.value = listFeatured
     }
 }
