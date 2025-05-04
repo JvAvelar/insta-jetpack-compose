d304acf0b24277c00c613d8dac57b517342fbb45
João Vitor Avelar <jvitoravelar1200@gmail.com>
2025-05-04 01:03:45 -0300

    implementado o NavGraph.kt para navegação e alterado os nomes dos métodos do viewModel

--- app/src/main/java/com/jvitor/jpcpratice/navigation/NavGraph.kt
+++ app/src/main/java/com/jvitor/jpcpratice/navigation/NavGraph.kt
@@ -5,9 +5,9 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.jvitor.jpcpratice.ui.view.components.Home
-import com.jvitor.jpcpratice.ui.view.components.PrimaryScreen
-import com.jvitor.jpcpratice.ui.view.components.SecondScreen
+import com.jvitor.jpcpratice.ui.view.screen.Home
+import com.jvitor.jpcpratice.ui.view.screen.PrimaryScreen
+import com.jvitor.jpcpratice.ui.view.screen.SecondScreen
 import com.jvitor.jpcpratice.viewmodel.ScreenViewModel
 
 @Composable
@@ -15,17 +15,14 @@ fun NavGraph() {
 
     val viewModel: ScreenViewModel = viewModel()
     val navController = rememberNavController()
+
     NavHost(navController, startDestination = "home") {
         composable("home") {
             Home(viewModel, navController)
-            viewModel.recuperarDestaque()
-            viewModel.recuperarPostagem()
+            viewModel.getFeatured()
+            viewModel.getPost()
         }
-        composable("primary") {
-            PrimaryScreen(navController)
-        }
-        composable("second") {
-            SecondScreen(navController)
-        }
+        composable("primary") { PrimaryScreen(navController) }
+        composable("second") { SecondScreen(navController) }
     }
 }
