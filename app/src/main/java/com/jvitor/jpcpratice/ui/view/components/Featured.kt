1f733948fed7f2b5bd17c8a40f65d914a92766f3
João Vitor Avelar <jvitoravelar1200@gmail.com>
2025-05-05 23:53:00 -0300

    renomeado o arquivo de ItemFeatured para Featured

--- app/src/main/java/com/jvitor/jpcpratice/ui/view/components/Featured.kt
+++ app/src/main/java/com/jvitor/jpcpratice/ui/view/components/Featured.kt
@@ -3,12 +3,15 @@ package com.jvitor.jpcpratice.ui.view.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -19,44 +22,54 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
+import com.jvitor.jpcpratice.data.model.FeatureModel
 import com.jvitor.jpcpratice.viewmodel.ScreenViewModel
 
 @Composable
-fun ItemFeatured(
+fun Featured(
     viewModel: ScreenViewModel,
-    navController: NavController,
     modifier: Modifier = Modifier
 ) {
     val listFeatured = viewModel.featured.collectAsState().value
 
     LazyRow(modifier.testTag("lazy_row_featured")) {
         items(listFeatured) { feat ->
-            Column(
-                modifier = Modifier.padding(8.dp).testTag("column_featured_items"),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Image(
-                    painter = painterResource(feat.profilePhoto),
-                    contentDescription = "profile photo",
-                    modifier = Modifier
-                        .testTag("img_profile_photo_featured")
-                        .size(56.dp)
-                        .background(Color.LightGray, CircleShape)
-                        .clip(CircleShape),
-                    contentScale = ContentScale.Crop
-                )
-
-                Text(
-                    text = feat.name,
-                    modifier = Modifier
-                        .padding(4.dp)
-                        .testTag("txt_user_name_featured")
-                )
+            Box {
+                ProfileFeatured(feat)
             }
         }
     }
 }
 
+@Composable
+private fun ProfileFeatured(feat: FeatureModel) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .testTag("column_featured_items"),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(feat.profilePhoto),
+            contentDescription = "profile photo",
+            modifier = Modifier
+                .testTag("img_profile_photo_featured")
+                .size(56.dp)
+                .background(Color.LightGray, CircleShape)
+                .clip(CircleShape),
+            contentScale = ContentScale.Crop
+        )
+
+        Text(
+            text = feat.name,
+            fontWeight = FontWeight.Normal,
+            modifier = Modifier
+                .padding(4.dp)
+                .testTag("txt_user_name_featured")
+        )
+    }
+}
