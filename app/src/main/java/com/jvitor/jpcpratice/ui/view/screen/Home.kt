a024f39fe8e9fe57fb340aaaa8d7580a7695e410
João Vitor Avelar <jvitoravelar1200@gmail.com>
2025-05-05 23:54:30 -0300

    refatorada no arquivo Home.kt

--- app/src/main/java/com/jvitor/jpcpratice/ui/view/screen/Home.kt
+++ app/src/main/java/com/jvitor/jpcpratice/ui/view/screen/Home.kt
@@ -1,17 +1,20 @@
 package com.jvitor.jpcpratice.ui.view.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.jvitor.jpcpratice.ui.view.components.BottomBar
-import com.jvitor.jpcpratice.ui.view.components.ItemFeatured
-import com.jvitor.jpcpratice.ui.view.components.ItemPost
+import com.jvitor.jpcpratice.ui.view.components.Featured
+import com.jvitor.jpcpratice.ui.view.components.Post
 import com.jvitor.jpcpratice.ui.view.components.TopBar
 import com.jvitor.jpcpratice.viewmodel.ScreenViewModel
 
@@ -31,9 +34,11 @@ fun Home(
 
         Column(modifier = Modifier.padding(innerPadding)) {
 
-            ItemFeatured(viewModel, navController, Modifier)
+            Featured(viewModel)
 
-            ItemPost(viewModel, navController, Modifier)
+            HorizontalDivider(modifier = Modifier.height(2.dp))
+
+            Post(viewModel, navController, Modifier)
         }
     }
 }
