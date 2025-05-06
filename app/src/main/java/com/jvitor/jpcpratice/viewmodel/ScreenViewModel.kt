3963096dccfddfae394d58be6d011528af72159d
João Vitor Avelar <jvitoravelar1200@gmail.com>
2025-05-05 23:53:43 -0300

    alteração dos nomes dos métodos na ScreenViewModel.kt

--- app/src/main/java/com/jvitor/jpcpratice/viewmodel/ScreenViewModel.kt
+++ app/src/main/java/com/jvitor/jpcpratice/viewmodel/ScreenViewModel.kt
@@ -2,39 +2,39 @@ package com.jvitor.jpcpratice.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.jvitor.jpcpratice.R
-import com.jvitor.jpcpratice.data.model.Feature
-import com.jvitor.jpcpratice.data.model.Post
+import com.jvitor.jpcpratice.data.model.FeatureModel
+import com.jvitor.jpcpratice.data.model.PostModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class ScreenViewModel : ViewModel() {
 
-    private val _post = MutableStateFlow<List<Post>>(emptyList())
-    val post: StateFlow<List<Post>> = _post.asStateFlow()
+    private val _post = MutableStateFlow<List<PostModel>>(emptyList())
+    val post: StateFlow<List<PostModel>> = _post.asStateFlow()
 
-    private val _featured = MutableStateFlow<List<Feature>>(emptyList())
-    val featured: StateFlow<List<Feature>> = _featured.asStateFlow()
+    private val _featured = MutableStateFlow<List<FeatureModel>>(emptyList())
+    val featured: StateFlow<List<FeatureModel>> = _featured.asStateFlow()
 
     fun getPost(){
         val listPost = listOf(
-            Post(R.drawable.perfil_01, "João", R.drawable.android, "Androidão é daora demais!", 2),
-            Post(R.drawable.perfil_02, "Luiz", R.drawable.floresta, "Visitei a floresta e foi muito Legal!"),
-            Post(R.drawable.perfil_03, "Luana", R.drawable.praia, "Visitei a praia e foi muito daora!", 2),
-            Post(R.drawable.perfil_01, "Zyon", R.drawable.android, "Android é bom demais!", 3),
+            PostModel(R.drawable.perfil_01, "João", R.drawable.android, "Androidão é daora demais!", 2),
+            PostModel(R.drawable.perfil_02, "Luiz", R.drawable.floresta, "Visitei a floresta e foi muito Legal!"),
+            PostModel(R.drawable.perfil_03, "Luana", R.drawable.praia, "Visitei a praia e foi muito daora!", 2),
+            PostModel(R.drawable.perfil_01, "Zyon", R.drawable.android, "Android é bom demais!", 3),
         )
         _post.value = listPost
     }
 
     fun getFeatured(){
         val listFeatured = listOf(
-            Feature(profilePhoto = R.drawable.perfil_01, name = "João"),
-            Feature(profilePhoto = R.drawable.perfil_02, name = "Luiz"),
-            Feature(profilePhoto = R.drawable.perfil_03, name = "Luana"),
-            Feature(profilePhoto = R.drawable.perfil_01, name = "Zyon"),
-            Feature(profilePhoto = R.drawable.perfil_02, name = "Vitor"),
-            Feature(profilePhoto = R.drawable.perfil_03, name = "Duarte"),
-            Feature(profilePhoto = R.drawable.perfil_01, name = "Avelar"),
+            FeatureModel(profilePhoto = R.drawable.perfil_01, name = "João"),
+            FeatureModel(profilePhoto = R.drawable.perfil_02, name = "Luiz"),
+            FeatureModel(profilePhoto = R.drawable.perfil_03, name = "Luana"),
+            FeatureModel(profilePhoto = R.drawable.perfil_01, name = "Zyon"),
+            FeatureModel(profilePhoto = R.drawable.perfil_02, name = "Vitor"),
+            FeatureModel(profilePhoto = R.drawable.perfil_03, name = "Duarte"),
+            FeatureModel(profilePhoto = R.drawable.perfil_01, name = "Avelar"),
         )
         _featured.value = listFeatured
     }
